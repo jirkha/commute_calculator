5f616fb26ff28ef9fcb1211fab1c6fca42611720
jirkha <jirkha13@seznam.cz>
2023-06-11 18:25:22 +0200

    fix error in saving the form for Railway deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { CounterContextProvider } from "./components/contexts/CounterContext";
 import Navbar from "./components/sections/Navbar";
 import GoogleMapsLoader from "./components/utils/GoogleMapsLoader";
